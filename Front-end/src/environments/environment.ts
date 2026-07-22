9e0eacc209f94a0d9cf705c506a1a6c93ab96e6c
matheus-czeck <matheusczeckcz@gmail.com>
2026-07-21 21:28:48 -0300

    fix: enviroments sem /

--- Front-end/src/environments/environment.ts
+++ Front-end/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://upideia-production.up.railway.app/',
+  apiUrl: 'https://upideia-production.up.railway.app',
   supabaseUrl: 'https://fkljgfhwksgdqhwurvdr.supabase.co',
   supabaseAnonKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrbGpnZmh3a3NnZHFod3VydmRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxNDMwOTYsImV4cCI6MjA5OTcxOTA5Nn0.A7p_sXmvz83Lu0qfP-TjlTDpRYWGvEZq0LgXT9krpws',
